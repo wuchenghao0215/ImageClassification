b945fcd612eb4fc32f457dcf673b8ae47234d2ab
wuchenghao0215 <wuchenghao0215@gmail.com>
2022-12-08 22:03:01 +0800

    feat(VGGNet)

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import torchvision.transforms as transforms
 # import matplotlib.pyplot as plt
 
 
-def load_data(batch_size=4):
+def load_data(batch_size=64):
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     # If running on Windows and you get a BrokenPipeError, 
@@ -44,7 +44,63 @@ class BaselineNet(nn.Module):
 
 
 # TODO define your net class here
-class MyNet(nn.Module):
+class MyVGGNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.block1_conv1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.block1_conv2 = nn.Conv2d(64, 64, 3, padding=1)
+
+        self.block2_conv1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.block2_conv2 = nn.Conv2d(128, 128, 3, padding=1)
+
+        self.block3_conv1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.block3_conv2 = nn.Conv2d(256, 256, 3, padding=1)
+
+        self.block4_conv1 = nn.Conv2d(256, 512, 3, padding=1)
+        self.block4_conv2 = nn.Conv2d(512, 512, 3, padding=1)
+
+        self.block5_conv1 = nn.Conv2d(512, 512, 3, padding=1)
+        self.block5_conv2 = nn.Conv2d(512, 512, 3, padding=1)
+
+        self.fc1 = nn.Linear(512 * 1 * 1, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 10)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.block1_conv1(x))
+        x = self.relu(self.block1_conv2(x))
+        x = self.pool(x)
+
+        x = self.relu(self.block2_conv1(x))
+        x = self.relu(self.block2_conv2(x))
+        x = self.pool(x)
+
+        x = self.relu(self.block3_conv1(x))
+        x = self.relu(self.block3_conv2(x))
+        x = self.pool(x)
+
+        x = self.relu(self.block4_conv1(x))
+        x = self.relu(self.block4_conv2(x))
+        x = self.pool(x)
+
+        x = self.relu(self.block5_conv1(x))
+        x = self.relu(self.block5_conv2(x))
+        x = self.pool(x)
+
+        x = torch.flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+
+        return x
+
+
+class MyResNet(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -59,6 +115,8 @@ def test(testloader, net, classes):
     with torch.no_grad():
         for data in testloader:
             images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
             # calculate outputs by running images through the network
             outputs = net(images)
             # the class with the highest energy is what we choose as prediction
@@ -75,6 +133,8 @@ def test(testloader, net, classes):
     with torch.no_grad():
         for data in testloader:
             images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
             outputs = net(images)
             _, predictions = torch.max(outputs, 1)
             # collect the correct predictions for each class
@@ -92,19 +152,22 @@ if __name__ == "__main__":
     # load data
     trainloader, testloader, classes = load_data()
     # init model
-    net = BaselineNet()
+    net = MyVGGNet()
     # use GPU acceleration
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net = net.to(device)
     # criterion and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(10):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -117,7 +180,7 @@ if __name__ == "__main__":
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:  # print every 2000 mini-batches
+            if i % 100 == 99:  # print every 2000 mini-batches
                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
                 running_loss = 0.0
 
